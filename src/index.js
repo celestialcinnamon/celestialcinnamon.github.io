baa7331ca314e16f1dc977e2178ea0a462b41426
Francis Rubio <francoisoibur21@gmail.com>
2018-11-08 17:08:25 +0800

    Last commit before release

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 'use strict'
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
+$$('.dialog').forEach(dialog => dialog.addEventListener('click', e => dialog.classList.remove('open')));
+$$('.dialog-2').forEach(dialog => dialog.addEventListener('click', e => dialog.classList.remove('open')));
+
+$$('.js-img').forEach(img => img.addEventListener('click', e => $('.dialog').classList.add('open')))
+$$('.js-img-2').forEach(img => img.addEventListener('click', e => $('.dialog-2').classList.add('open')))
