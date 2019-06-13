116129e63b1389a19571b77f572c238000521186
Francis Rubio <celestialcinnamon@github.com>
2019-06-13 20:22:19 +0800

    Finished design screen

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 particlesJS.load("particles-js", "/src/particles.json");
 particlesJS.load("particles-1-js", "/src/particles.1.json");
+
+(function() {
+    const canvas = document.getElementById('decoration')
+    const ctx = canvas.getContext('2d');
+
+    console.log("Setting color")
+    console.log("Setting color")
+    ctx.beginPath();
+    ctx.strokeStyle = "#fff"
+    ctx.moveTo(10, 0)
+    ctx.lineTo(30, 50)
+    ctx.stroke();
+    ctx.closePath()
+})();
